cd660ab6016b71fd1bece0c8eea15b5ad8b148ba
Lilian G <97319339+gimenezt@users.noreply.github.com>
2023-04-10 12:02:14 -0300

    Create CListNode.java

---
+++ Algorithms and Data Structure/ADS III/Node Lists/CList/CListNode.java
@@ -0,0 +1,29 @@
+package uscs;
+
+public class CListNode {
+    int item;
+    CListNode next;
+    CListNode prev;
+
+    //constrtur para node nulo
+    public CListNode(){
+        this.item = 0;
+        this.next = null;
+        this.prev = null;
+    }
+
+    //constrtor para lista com node único (sem prev nem next)
+    public CListNode(int item){
+        this.item = item;
+        this.next = null;
+        this.prev = null;
+    }
+    //constrtor para novo node completo
+    public CListNode(int item, CListNode next, CListNode prev){
+        this.item = item;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    
+}
